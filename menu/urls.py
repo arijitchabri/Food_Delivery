b55d1208ebc14e2be92c2b06a64aa14b0647ee60
arijit <arijitchabri2001@gmail.com>
2023-04-26 07:03:30 +0530

    Filtering through resturant added.

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('resturant_search/<str:rest>', views.resturant_search, name = 'resturant_search')
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from . models import * 
-from . forms import *
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
     dish = Dish.objects.all()
     context = {
         'dish' : dish
+    }
+    return render(request, 'menu/index.html', context = context)
+
+def resturant_search(request, rest):
+    dish = []
+    for i in Dish.objects.all():
+        resturant = str(i.resturant)
+        if resturant == rest:
+            dish.append(i)
+    context = {
+        'dish' : dish,
+        'resturant' : rest
     }
     return render(request, 'menu/index.html', context = context)
