3366578b726f807de615c7c6ac8fddcff08debb7
arijit <arijitchabri2001@gmail.com>
2023-04-27 12:03:17 +0530

    Handling the some exceptions and documentation

--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Tags, Dish, Designation, Customer
+from .models import Tags
 from .forms import *
 
 
@@ -16,24 +15,27 @@ def index(request):
     user = request.user         # Collecting the requested user.
     user_name = str(user)       # Logged in users name as string.
     try:
-        customer = Customer.objects.get(user = user)    # Collecting the customer.
+        customer = Customer.objects.get(user=user)    # Collecting the customer.
         designation = str(customer.designation)         # Designation of logged in customer.
-    except TypeError:           # If its an anonymous user the he will have no designation.
+    except TypeError:             # If its an anonymous user the he will have no designation.
         context = {
-            'dish' : dish,          # All available dishes.
-            'user' : user_name,     # Logged in user's name as a string.
+            'dish': dish,          # All available dishes.
+            'user': user_name,     # Logged in user's name as a string.
         }
         return render(request, 'menu/index.html', context=context)
+    except Customer.DoesNotExist:
+        messages.error(request, 'Create the customer account.')
+        return redirect('customer_creation')
 
     #  For general user all the dishes will show.
     # If the requested user is a Restaurant then only its menu will appear.
 
     if designation == 'Restaurant':
-        dish = Dish.objects.filter(restaurant = user)
+        dish = Dish.objects.filter(restaurant=user)
     context = {
         'dish': dish,                       # All the viewable dishes.
         'user': user_name,                  # Logged in user's name as a string.
-        'designation' : designation,        # Designation of the user.
+        'designation': designation,        # Designation of the user.
     }
     print(designation, type(designation))
     return render(request, 'menu/index.html', context=context)
@@ -43,7 +45,7 @@ def restaurant_search(request, rest):
     # Filtering through specific restaurant.
 
     restaurant = User.objects.get(username=rest)            # Collecting the restaurant.
-    dish = Dish.objects.filter(restaurant = restaurant)     # Filtering the dishes.
+    dish = Dish.objects.filter(restaurant=restaurant)     # Filtering the dishes.
     user = request.user
     user_name = str(user)
     context = {
@@ -56,62 +58,69 @@ def restaurant_search(request, rest):
 
 def tag_search(request, tag):
     # Filtering through tag.
-    tag = Tags.objects.get(tag = tag)
-    dish = Dish.objects.filter(tags = tag)
+    tag = Tags.objects.get(tag=tag)
+    dish = Dish.objects.filter(tags=tag)
     user = request.user
     user_name = str(user)
     try:
-        customer = Customer.objects.get(user = user)    # Collecting the customer.
+        customer = Customer.objects.get(user=user)    # Collecting the customer.
         designation = str(customer.designation)         # Designation of logged in customer.
     except TypeError:           # If its an anonymous user the he will have no designation.
         context = {
-            'dish' : dish,          # All available dishes.
-            'user' : user_name,     # Logged in user's name as a string.
+            'dish': dish,          # All available dishes.
+            'user': user_name,     # Logged in user's name as a string.
         }
         return render(request, 'menu/index.html', context=context)
     if designation == 'Restaurant':
-        # If the designation is resturant then it will filter all the dishes of that restaurant with the same tag.
-        dish = Dish.objects.filter(restaurant = user, tags = tag)
+        # If the designation is restaurant then it will filter all the dishes of that restaurant with the same tag.
+        dish = Dish.objects.filter(restaurant=user, tags=tag)
     context = {
         'dish': dish,
         'tag': tag,
         'user': user_name,
-        'designation' : designation,
+        'designation': designation,
     }
     return render(request, 'menu/index.html', context=context)
 
+
 @login_required
 def dish_creation(request):
-    form = Dish_creation_Form
-    user = request.user
+    # Creating dish by using the forms
+    form = Dish_creation_Form                    # Storing the empty form
+    user = request.user                          # Logged in user
+    customer = Customer.objects.get(user=user)
+    if customer.designation != 'Restaurant':
+        # If the user is not a restaurant he can not add a dish.
+        messages.error(request, 'Only Restaurants can add dish.')
+        return redirect('index')
     if request.method == 'POST':
-        form = Dish_creation_Form(request.POST, request.FILES)
-        if form.is_valid():
-            form = form.save(commit=False)
-            form.restaurant = user
-            form.save()
+        form = Dish_creation_Form(request.POST, request.FILES)      # Storing the submitted form
+        if form.is_valid():                                         # Validating the form
+            form = form.save(commit=False)                          # Saving the form but not submitting yet
+            form.restaurant = user                                  # Adding the user
+            form.save()                                             # Saving the form
             messages.success(request, 'Dish added successfully.')
             return redirect('index')
     context = {
-        'form_type' : 'dish_creation',
-        'form' : form
+        'form_type': 'dish_creation',
+        'form': form,
     }
-    return render(request, 'menu/dish.html', context = context)
+    return render(request, 'menu/dish.html', context=context)     # Before submitting the form
 
 
 @login_required
 def dish_modification(request, dish_id):
-    dish = Dish.objects.get(id = dish_id)
+    dish = Dish.objects.get(id=dish_id)
     user = request.user
     restaurant = user
-    form = Dish_creation_Form(instance=dish)
-    if str(restaurant) == str(dish.restaurant):
+    form = Dish_creation_Form(instance=dish)        # Storing the dish which we want to modify.
+    if str(restaurant) == str(dish.restaurant):     # Dish modification can only be done by the owner restaurant.
         if request.method == 'POST':
-            form = Dish_creation_Form(request.POST, request.FILES, instance = dish)
+            form = Dish_creation_Form(request.POST, request.FILES, instance=dish)
             if form.is_valid():
                 form = form.save(commit=False)
                 form.restaurant = user
-                form.save()
+                form.save()                         # Dish has been modified and saved.
                 messages.success(request, 'Dish modified successfully.')
                 return redirect('index')
     else:
@@ -119,8 +128,8 @@ def dish_modification(request, dish_id):
         return redirect('index')
     context = {
         'form_type': 'dish_modification',
-        'id' : dish_id,
-        'form' : form,
+        'id': dish_id,
+        'form': form,
 
     }
     return render(request, 'menu/dish.html', context=context)
@@ -128,12 +137,12 @@ def dish_modification(request, dish_id):
 
 @login_required
 def dish_deletion(request, dish_id):
-    dish = Dish.objects.get(id = dish_id)
+    dish = Dish.objects.get(id=dish_id)
     user = request.user
     restaurant = user
-    if str(restaurant) == str(dish.restaurant):
+    if str(restaurant) == str(dish.restaurant):         # Dish deletion can only be done by the owner restaurant.
         if request.method == 'POST':
-            dish.delete()
+            dish.delete()                               # Deleting the dish.
             messages.success(request, 'Dish deleted successfully.')
             return redirect('index')
     else:
@@ -142,24 +151,25 @@ def dish_deletion(request, dish_id):
 
     context = {
         'form_type': 'dish_deletion',
-        'id' : dish_id,
-        'dish' : dish,
+        'id': dish_id,
+        'dish': dish,
     }
     return render(request, 'menu/dish.html', context=context)
 
 
-
 def user_creation(request):
-    form = UserCreationForm
+    # A new user has to be created before adding a new customer..
+
+    form = UserCreationForm             # Storing the UserCreation form provided by Django
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            form.save()                 # If the form is valid and okay it saved.
             username = request.POST['username']
             password = request.POST['password1']
-            user = authenticate(request, username=username, password=password)
-            login(request, user)
-        return redirect('customer_creation')
+            user = authenticate(request, username=username, password=password)      # Authenticating the user.
+            login(request, user)        # Logging in by the newly created user.
+        return redirect('customer_creation')    # Redirecting for creating the customer profile for this user.
     context = {
         'form': form,
     }
@@ -168,15 +178,15 @@ def user_creation(request):
 
 @login_required
 def customer_creation(request):
-    form = Customer_form
+    form = Customer_form                    # Storing the Customer creation form.
     if request.method == 'POST':
-        form = Customer_form(request.POST)
-        if form.is_valid():
-            form = form.save(commit=False)
-            user = User.objects.get(username=request.user)
-            form.user = user
+        form = Customer_form(request.POST)  # Storing the send data in the form.
+        if form.is_valid():                 # Validating the form.
+            form = form.save(commit=False)  # If the from is validated okay then its been saved but not stored.
+            user = User.objects.get(username=request.user)  # Retrieving the logged in user.
+            form.user = user                # Adding the user in the form.
             form.save()
-            messages.success(request,'Your account is created successfully.')
+            messages.success(request, 'Your account is created successfully.')
         return redirect('index')
     context = {
         'form': form,
@@ -186,28 +196,31 @@ def customer_creation(request):
 
 
 def log_out(request):
+    # Logout request
     logout(request)
     messages.success(request, 'You are successfully logged out')
     return redirect('index')
 
 
 def log_in(request):
+    # Login request
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST['username']     # Storing the username
+        password = request.POST['password']     # Storing the password
         try:
             user = User.objects.get(username=username)
-        except:
-            messages.error(request, 'user not exits')
-            return redirect('log_in')
+            print('Logging in as', user)
+        except User.DoesNotExist:
+            messages.error(request, 'user does not exits')
+            return redirect('log_in')           # If user does not exists redirected to login page
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)  # Authenticating the user.
 
         if user:
             messages.success(request, 'login successfully')
-            login(request, user)
+            login(request, user)                # Logging in the authenticated user.
             return redirect('index')
-        else:
+        else:                                   # If credentials are false redirecting to login page.
             messages.error(request, 'username or password is incorrect')
             return redirect('log_in')
     return render(request, 'menu/log_in.html')
